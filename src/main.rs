881013efe67558bdc9ec3d3ce88f8816daada5d4
Mark Biesheuvel <mail@markbiesheuvel.nl>
2022-10-11 13:39:01 +0200

    Create enum for Piece

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,67 @@
 #[derive(Debug)]
-struct Piece {
-    piece_type: char,
-    has_moved: bool,
+enum Piece {
+    Pawn { has_moved: bool },
+    Knight,
+    Bishop,
+    Rook { has_moved: bool },
+    Queen,
+    King { has_moved: bool },
 }
 
 impl Piece {
-    fn new(piece_type: char) -> Self {
-        Self {
-            piece_type,
-            has_moved: false,
+    fn make_move(&mut self) {
+        // Update the has_moved property on pieces with special moves, ignore for anything else
+        match self {
+            Piece::King { has_moved } | Piece::Pawn { has_moved } | Piece::Rook { has_moved } => {
+                *has_moved = true;
+            }
+            _ => {}
         }
     }
 
-    fn make_move(&mut self) {
-        self.has_moved = true;
-    }
-
+    // TODO: create tuple struct for move
     fn legal_moves(&self) -> Vec<(i8, i8)> {
-        // TODO: create tuple struct for move
-        // TODO: implement for other pieces
-        vec![
-            (2, 1),
-            (2, -1),
-            (-2, 1),
-            (-2, -1),
-            (1, 2),
-            (1, -2),
-            (-1, 2),
-            (-1, 2),
-        ]
+        match self {
+            Piece::Knight => {
+                vec![
+                    (2, 1),
+                    (2, -1),
+                    (-2, 1),
+                    (-2, -1),
+                    (1, 2),
+                    (1, -2),
+                    (-1, 2),
+                    (-1, 2),
+                ]
+            }
+            _ => {
+                // TODO: implement for other pieces
+                vec![]
+            }
+        }
     }
 }
 
 fn main() {
-    let mut rook = Piece::new('R');
-    let bishop = Piece::new('B');
-    let knight = Piece::new('N');
+    let mut rook = Piece::Rook { has_moved: false };
+    let mut bishop = Piece::Bishop;
+    let mut knight = Piece::Knight;
+    let _pawn = Piece::Pawn { has_moved: false };
+    let _king = Piece::King { has_moved: false };
+    let _queen = Piece::Queen;
 
-    // Move rook
+    // Move all pieces
     rook.make_move();
+    bishop.make_move();
+    knight.make_move();
 
     // Print all pieces
     println!("{:?}", rook);
     println!("{:?}", bishop);
     println!("{:?}", knight);
 
-    // Show legal moves of knight
+    // Show legal moves of all pieces
+    println!("{:?}", rook.legal_moves());
+    println!("{:?}", bishop.legal_moves());
     println!("{:?}", knight.legal_moves());
-
-    // Avoid dead code warning
-    bishop.piece_type;
-    bishop.has_moved;
 }
