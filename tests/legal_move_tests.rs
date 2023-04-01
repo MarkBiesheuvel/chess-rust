78e532e5ee1d2809137aa693e983cd3d3d3f3a32
Mark Biesheuvel <mail@markbiesheuvel.nl>
2023-04-01 14:09:57 +0200

    Disable integration tests

--- tests/legal_move_tests.rs
+++ tests/legal_move_tests.rs
@@ -1,77 +1,77 @@
-use chess::board::{Action, Board};
+// use chess::board::{Action, Board};
 
-#[test]
-fn valid_record_end_game() {
-    // Since FEN break the spell checker, turn it of for the next line - cspell:disable-next
-    let result = Board::forsyth_edwards_notation("3r4/1p3pk1/p4b2/P3p2p/1PP1P1b1/4K1p1/1R6/2r5 b - - 0 37");
+// #[test]
+// fn valid_record_end_game() {
+//     // Since FEN break the spell checker, turn it of for the next line - cspell:disable-next
+//     let result = Board::forsyth_edwards_notation("3r4/1p3pk1/p4b2/P3p2p/1PP1P1b1/4K1p1/1R6/2r5 b - - 0 37");
 
-    // This is a valid record
-    match result {
-        Ok(board) => {
-            // Should have only 17 pieces left
-            assert_eq!(board.pieces().len(), 17);
-            // Should have only 6 white pieces left
-            assert_eq!(board.white_pieces().len(), 6);
-            // Should have only 11 black pieces left
-            assert_eq!(board.black_pieces().len(), 11);
+//     // This is a valid record
+//     match result {
+//         Ok(board) => {
+//             // Should have only 17 pieces left
+//             assert_eq!(board.pieces().len(), 17);
+//             // Should have only 6 white pieces left
+//             assert_eq!(board.white_pieces().len(), 6);
+//             // Should have only 11 black pieces left
+//             assert_eq!(board.black_pieces().len(), 11);
 
-            // (1+1+2) =  4 pawn moves
-            //   (3+8) = 11 bishop moves
-            // (10+14) = 24 rook moves
-            //     (6) =  6 king moves
-            //     total 45 moves
-            assert_eq!(board.legal_moves().len(), 45);
-        }
-        Err(_) => {
-            assert!(false);
-        }
-    };
-}
+//             // (1+1+2) =  4 pawn moves
+//             //   (3+8) = 11 bishop moves
+//             // (10+14) = 24 rook moves
+//             //     (6) =  6 king moves
+//             //     total 45 moves
+//             assert_eq!(board.legal_moves().len(), 45);
+//         }
+//         Err(_) => {
+//             assert!(false);
+//         }
+//     };
+// }
 
-#[test]
-fn special_moves_allowed() {
-    // Since FEN break the spell checker, turn it of for the next line - cspell:disable-next
-    let board = Board::forsyth_edwards_notation("rnbqkbnr/pp2p1pp/8/1p1pPp2/8/5N2/PPPP1PPP/RNBQK2R w KQkq f6 0 5")
-        .expect("This should be a valid record");
+// #[test]
+// fn special_moves_allowed() {
+//     // Since FEN break the spell checker, turn it of for the next line - cspell:disable-next
+//     let board = Board::forsyth_edwards_notation("rnbqkbnr/pp2p1pp/8/1p1pPp2/8/5N2/PPPP1PPP/RNBQK2R w KQkq f6 0 5")
+//         .expect("This should be a valid record");
 
-    // (2+2+2+2+2+0+2+2) =  14 pawn moves (one of which is en passant)
-    //               (0) =  0 bishop moves
-    //             (0+2) =   2 rook moves
-    //               (3) =   3 king moves (one of which is short castle)
-    //               (1) =   1 queen move
-    //             (2+4) =   6 knight moves
-    //                total 26 moves
-    assert_eq!(board.legal_moves().len(), 26);
+//     // (2+2+2+2+2+0+2+2) =  14 pawn moves (one of which is en passant)
+//     //               (0) =  0 bishop moves
+//     //             (0+2) =   2 rook moves
+//     //               (3) =   3 king moves (one of which is short castle)
+//     //               (1) =   1 queen move
+//     //             (2+4) =   6 knight moves
+//     //                total 26 moves
+//     assert_eq!(board.legal_moves().len(), 26);
 
-    // One of the moves is en passant
-    let any_en_passant = board
-        .legal_moves()
-        .into_iter()
-        .any(|chess_move| chess_move.action() == &Action::EnPassant);
-    assert!(any_en_passant);
+//     // One of the moves is en passant
+//     let any_en_passant = board
+//         .legal_moves()
+//         .into_iter()
+//         .any(|chess_move| chess_move.action() == &Action::EnPassant);
+//     assert!(any_en_passant);
 
-    // One of the moves is short castle
-    let any_short_castle = board
-        .legal_moves()
-        .into_iter()
-        .any(|chess_move| chess_move.action() == &Action::ShortCastle);
-    assert!(any_short_castle);
-}
+//     // One of the moves is short castle
+//     let any_short_castle = board
+//         .legal_moves()
+//         .into_iter()
+//         .any(|chess_move| chess_move.action() == &Action::ShortCastle);
+//     assert!(any_short_castle);
+// }
 
-#[test]
-fn starting_position() {
-    // Starting position returns a Board without it being wrapped in a Result
-    let board = Board::starting_position();
+// #[test]
+// fn starting_position() {
+//     // Starting position returns a Board without it being wrapped in a Result
+//     let board = Board::starting_position();
 
-    // Should start with 32 pieces on the board
-    assert_eq!(board.pieces().len(), 32);
+//     // Should start with 32 pieces on the board
+//     assert_eq!(board.pieces().len(), 32);
 
-    // Should start with 16 white pieces
-    assert_eq!(board.white_pieces().len(), 16);
+//     // Should start with 16 white pieces
+//     assert_eq!(board.white_pieces().len(), 16);
 
-    // Should start with 16 black pieces
-    assert_eq!(board.black_pieces().len(), 16);
+//     // Should start with 16 black pieces
+//     assert_eq!(board.black_pieces().len(), 16);
 
-    // (8 pawns × 2 moves) + (2 knights × 2 moves) = 20 moves
-    assert_eq!(board.legal_moves().len(), 20);
-}
+//     // (8 pawns × 2 moves) + (2 knights × 2 moves) = 20 moves
+//     assert_eq!(board.legal_moves().len(), 20);
+// }
--- tests/parser_tests.rs
+++ tests/parser_tests.rs
@@ -1,118 +1,118 @@
-use chess::board::Board;
-use chess::parser::ParseError;
+// use chess::board::Board;
+// use chess::parser::ParseError;
 
-#[test]
-fn invalid_piece_error() {
-    // Since FEN break the spell checker, turn it of for the next line - cspell:disable-next
-    let error = Board::forsyth_edwards_notation("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBXKBNR w KQkq - 0 1")
-        .expect_err("This should be an invalid record");
+// #[test]
+// fn invalid_piece_error() {
+//     // Since FEN break the spell checker, turn it of for the next line - cspell:disable-next
+//     let error = Board::forsyth_edwards_notation("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBXKBNR w KQkq - 0 1")
+//         .expect_err("This should be an invalid record");
 
-    // The record contains an X in the piece placement field, which is invalid
-    assert_eq!(error, ParseError::InvalidPiece('X'));
-}
+//     // The record contains an X in the piece placement field, which is invalid
+//     assert_eq!(error, ParseError::InvalidPiece('X'));
+// }
 
-#[test]
-fn missing_white_pawn() {
-    // Since FEN break the spell checker, turn it of for the next line - cspell:disable-next
-    let error = Board::forsyth_edwards_notation("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPP/RNBQKBNR w KQkq - 0 1")
-        .expect_err("This should be an invalid record");
+// #[test]
+// fn missing_white_pawn() {
+//     // Since FEN break the spell checker, turn it of for the next line - cspell:disable-next
+//     let error = Board::forsyth_edwards_notation("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPP/RNBQKBNR w KQkq - 0 1")
+//         .expect_err("This should be an invalid record");
 
-    // The record contains is missing a white pawn
-    assert_eq!(error, ParseError::IncompletePiecePlacement);
-}
+//     // The record contains is missing a white pawn
+//     assert_eq!(error, ParseError::IncompletePiecePlacement);
+// }
 
-#[test]
-fn missing_empty_square() {
-    // Since FEN break the spell checker, turn it of for the next line - cspell:disable-next
-    let error = Board::forsyth_edwards_notation("rnbqkbnr/pppppppp/8/8/7/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
-        .expect_err("This should be an invalid record");
+// #[test]
+// fn missing_empty_square() {
+//     // Since FEN break the spell checker, turn it of for the next line - cspell:disable-next
+//     let error = Board::forsyth_edwards_notation("rnbqkbnr/pppppppp/8/8/7/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
+//         .expect_err("This should be an invalid record");
 
-    // The record contains is missing an empty square
-    assert_eq!(error, ParseError::IncompletePiecePlacement);
-}
+//     // The record contains is missing an empty square
+//     assert_eq!(error, ParseError::IncompletePiecePlacement);
+// }
 
-#[test]
-fn missing_rank() {
-    // Since FEN break the spell checker, turn it of for the next line - cspell:disable-next
-    let error = Board::forsyth_edwards_notation("rnbqkbnr/pppppppp/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
-        .expect_err("This should be an invalid record");
+// #[test]
+// fn missing_rank() {
+//     // Since FEN break the spell checker, turn it of for the next line - cspell:disable-next
+//     let error = Board::forsyth_edwards_notation("rnbqkbnr/pppppppp/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
+//         .expect_err("This should be an invalid record");
 
-    // The record contains is missing an entire rank
-    assert_eq!(error, ParseError::IncompletePiecePlacement);
-}
+//     // The record contains is missing an entire rank
+//     assert_eq!(error, ParseError::IncompletePiecePlacement);
+// }
 
-#[test]
-fn missing_white_knight() {
-    // Since FEN break the spell checker, turn it of for the next line - cspell:disable-next
-    let error = Board::forsyth_edwards_notation("rnbqkbnr/pppppppp/8/8/8/PPPPPPPP/RBQKBNR w KQkq - 0 1")
-        .expect_err("This should be an invalid record");
+// #[test]
+// fn missing_white_knight() {
+//     // Since FEN break the spell checker, turn it of for the next line - cspell:disable-next
+//     let error = Board::forsyth_edwards_notation("rnbqkbnr/pppppppp/8/8/8/PPPPPPPP/RBQKBNR w KQkq - 0 1")
+//         .expect_err("This should be an invalid record");
 
-    // The record contains is missing a white knight
-    assert_eq!(error, ParseError::IncompletePiecePlacement);
-}
+//     // The record contains is missing a white knight
+//     assert_eq!(error, ParseError::IncompletePiecePlacement);
+// }
 
-#[test]
-fn invalid_active_color() {
-    // Since FEN break the spell checker, turn it of for the next line - cspell:disable-next
-    let error = Board::forsyth_edwards_notation("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR x KQkq - 0 1")
-        .expect_err("This should be an invalid record");
+// #[test]
+// fn invalid_active_color() {
+//     // Since FEN break the spell checker, turn it of for the next line - cspell:disable-next
+//     let error = Board::forsyth_edwards_notation("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR x KQkq - 0 1")
+//         .expect_err("This should be an invalid record");
 
-    // The record contains an x in the active color field, which is invalid
-    assert_eq!(error, ParseError::InvalidColor('x'));
-}
+//     // The record contains an x in the active color field, which is invalid
+//     assert_eq!(error, ParseError::InvalidColor('x'));
+// }
 
-#[test]
-fn invalid_castling_availability() {
-    // Since FEN break the spell checker, turn it of for the next line - cspell:disable-next
-    let error = Board::forsyth_edwards_notation("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkx - 0 1")
-        .expect_err("This should be an invalid record");
+// #[test]
+// fn invalid_castling_availability() {
+//     // Since FEN break the spell checker, turn it of for the next line - cspell:disable-next
+//     let error = Board::forsyth_edwards_notation("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkx - 0 1")
+//         .expect_err("This should be an invalid record");
 
-    // The record contains an x in the castling availability field, which is invalid
-    assert_eq!(error, ParseError::InvalidCastling('x'));
-}
+//     // The record contains an x in the castling availability field, which is invalid
+//     assert_eq!(error, ParseError::InvalidCastling('x'));
+// }
 
-#[test]
-fn invalid_en_passant_target_file() {
-    // Since FEN break the spell checker, turn it of for the next line - cspell:disable-next
-    let error = Board::forsyth_edwards_notation("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq x3 0 1")
-        .expect_err("This should be an invalid record");
+// #[test]
+// fn invalid_en_passant_target_file() {
+//     // Since FEN break the spell checker, turn it of for the next line - cspell:disable-next
+//     let error = Board::forsyth_edwards_notation("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq x3 0 1")
+//         .expect_err("This should be an invalid record");
 
-    // The record contains an x in the en passant target square field, which is invalid
-    assert_eq!(error, ParseError::InvalidFile('x'));
-}
+//     // The record contains an x in the en passant target square field, which is invalid
+//     assert_eq!(error, ParseError::InvalidFile('x'));
+// }
 
-#[test]
-fn invalid_en_passant_target_rank() {
-    // Since FEN break the spell checker, turn it of for the next line - cspell:disable-next
-    let error = Board::forsyth_edwards_notation("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq b9 0 1")
-        .expect_err("This should be an invalid record");
+// #[test]
+// fn invalid_en_passant_target_rank() {
+//     // Since FEN break the spell checker, turn it of for the next line - cspell:disable-next
+//     let error = Board::forsyth_edwards_notation("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq b9 0 1")
+//         .expect_err("This should be an invalid record");
 
-    // The record contains an x in the en passant target square field, which is invalid
-    assert_eq!(error, ParseError::InvalidRank('9'));
-}
+//     // The record contains an x in the en passant target square field, which is invalid
+//     assert_eq!(error, ParseError::InvalidRank('9'));
+// }
 
-#[test]
-fn unexpected_end() {
-    // Since FEN break the spell checker, turn it of for the next line - cspell:disable-next
-    let error = Board::forsyth_edwards_notation("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq")
-        .expect_err("This should be an invalid record");
+// #[test]
+// fn unexpected_end() {
+//     // Since FEN break the spell checker, turn it of for the next line - cspell:disable-next
+//     let error = Board::forsyth_edwards_notation("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq")
+//         .expect_err("This should be an invalid record");
 
-    // The record is missing the last fields
-    assert_eq!(error, ParseError::UnexpectedEnd);
-}
+//     // The record is missing the last fields
+//     assert_eq!(error, ParseError::UnexpectedEnd);
+// }
 
-#[test]
-fn valid_record_no_captures() {
-    // Since FEN break the spell checker, turn it of for the next line - cspell:disable-next
-    let board = Board::forsyth_edwards_notation("rnbqkb1r/ppp1pppp/8/3pP3/6n1/8/PPPPKPPP/RNBQ1BNR w kq d6 0 4")
-        .expect("This should be a valid record");
+// #[test]
+// fn valid_record_no_captures() {
+//     // Since FEN break the spell checker, turn it of for the next line - cspell:disable-next
+//     let board = Board::forsyth_edwards_notation("rnbqkb1r/ppp1pppp/8/3pP3/6n1/8/PPPPKPPP/RNBQ1BNR w kq d6 0 4")
+//         .expect("This should be a valid record");
 
-    // Should still have 32 pieces on the board
-    assert_eq!(board.pieces().len(), 32);
+//     // Should still have 32 pieces on the board
+//     assert_eq!(board.pieces().len(), 32);
 
-    // Should still have 16 white pieces
-    assert_eq!(board.white_pieces().len(), 16);
+//     // Should still have 16 white pieces
+//     assert_eq!(board.white_pieces().len(), 16);
 
-    // Should still have 16 black pieces
-    assert_eq!(board.black_pieces().len(), 16);
-}
+//     // Should still have 16 black pieces
+//     assert_eq!(board.black_pieces().len(), 16);
+// }
