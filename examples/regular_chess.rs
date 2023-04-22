5ed0f95152c6d1e5a77e884f7a619863e9acc1dd
Mark Biesheuvel <mail@markbiesheuvel.nl>
2023-04-22 21:27:42 +0200

    Build tiny example

--- examples/regular_chess.rs
+++ examples/regular_chess.rs
@@ -1,5 +1,18 @@
 use std::error::Error;
+use chess::piece::Piece;
+use chess::piece::Color::{Black, White};
+use chess::piece::behavior::{Knight, Queen};
 
 fn main() -> Result<(), Box<dyn Error>> {
+    let white_knight = Piece::new(White, Knight);
+    let black_queen = Piece::new(Black, Queen);
+    
+    let collection = Vec::from([
+        white_knight,
+        black_queen,
+    ]);
+
+    dbg!(collection);
+
     Ok(())
 }
